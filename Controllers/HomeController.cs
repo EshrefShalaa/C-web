d09e19107895d08f4dea7c16f852736d8c3f4ba1
EshrefShalaa <esh.shala@hotmail.com>
2023-01-22 18:28:32 +0100

    done

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Calculate(int num1, int num2)
+        {
+            this.ViewBag.Num1 = num1;
+            this.ViewBag.Num2 = num2;
+            this.ViewBag.sum = num1 + num2;
+            return View("Index");
+        }
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        
+    }
+    
+}
